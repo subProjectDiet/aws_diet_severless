0399e27adf949344983b72bb5beb707103faa38d
yugyeong1 <dbrud1032@gmail.com>
2023-03-18 22:42:47 +0900

    add

--- resources/recommend.py
+++ resources/recommend.py
@@ -1,61 +1,61 @@
-from datetime import datetime
-from flask import request
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
-from flask_restful import Resource
-from mysql.connector import Error
+# from datetime import datetime
+# from flask import request
+# from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+# from flask_restful import Resource
+# from mysql.connector import Error
 
-from mysql_connection import get_connection
-from email_validator import validate_email, EmailNotValidError
-from flask_jwt_extended import get_jwt_identity
-
-
-class KmeansRecommendResource(Resource):
-    @jwt_required()
-    def get(self):
-
-        group = request.args.get('group')
-
-        try : 
-            connection = get_connection()
-            query = '''select p.id as postingId, ui.userId, u.nickName,  p.content, p.imgUrl, p.createdAt, p.updatedAt, count(l.postingId) as likeCnt, ui.`group`
-                   from userInfo ui
-                    join user u
-                    on ui.userId = u.id
-                    left join posting p
-                    on ui.userId = p.userId
-                    join likePosting l
-                    on p.id = l.postingId
-                    where `group` = %s
-                    group by l.postingId
-                    order by likeCnt desc
-                    limit 1;
-                    '''
-
-            record = (group, )
-            cursor = connection.cursor(dictionary=True)
-            cursor.execute(query, record)
-
-            result_list = cursor.fetchall()
-
-            i = 0
-            for row in result_list :
-                result_list[i]['createdAt'] = row['createdAt'].isoformat()
-                result_list[i]['updatedAt'] = row['updatedAt'].isoformat()
-
-                i = i + 1
-
-            cursor.close()
-            connection.close()
-
-        except Error as e :
-            print(e)
-            cursor.close()
-            connection.close()
-            return {'error' : str(e)} , 500
+# from mysql_connection import get_connection
+# from email_validator import validate_email, EmailNotValidError
+# from flask_jwt_extended import get_jwt_identity
 
 
-        return {'result' : 'success',
-                'items' : result_list[0] ,
-                'count' : len(result_list)  }, 200
+# class KmeansRecommendResource(Resource):
+#     @jwt_required()
+#     def get(self):
+
+#         group = request.args.get('group')
+
+#         try : 
+#             connection = get_connection()
+#             query = '''select p.id as postingId, ui.userId, u.nickName,  p.content, p.imgUrl, p.createdAt, p.updatedAt, count(l.postingId) as likeCnt, ui.`group`
+#                    from userInfo ui
+#                     join user u
+#                     on ui.userId = u.id
+#                     left join posting p
+#                     on ui.userId = p.userId
+#                     join likePosting l
+#                     on p.id = l.postingId
+#                     where `group` = %s
+#                     group by l.postingId
+#                     order by likeCnt desc
+#                     limit 1;
+#                     '''
+
+#             record = (group, )
+#             cursor = connection.cursor(dictionary=True)
+#             cursor.execute(query, record)
+
+#             result_list = cursor.fetchall()
+
+#             i = 0
+#             for row in result_list :
+#                 result_list[i]['createdAt'] = row['createdAt'].isoformat()
+#                 result_list[i]['updatedAt'] = row['updatedAt'].isoformat()
+
+#                 i = i + 1
+
+#             cursor.close()
+#             connection.close()
+
+#         except Error as e :
+#             print(e)
+#             cursor.close()
+#             connection.close()
+#             return {'error' : str(e)} , 500
+
+
+#         return {'result' : 'success',
+#                 'items' : result_list[0] ,
+#                 'count' : len(result_list)  }, 200
 
 
